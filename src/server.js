7515e3e8708ffd059efede854281c1de7ded5dad
Edson Carlos <edsoncarlos@ymail.com>
2026-02-04 15:08:43 -0300

    CHORE: Adding message about door

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
+  console.log("Servidor rodando na porta 8000")
   console.log("API disponível em http://localhost:8000")
 })
